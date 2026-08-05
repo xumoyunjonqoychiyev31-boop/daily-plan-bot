cb4abb444eb6c2d791a4975c22e7440e35740950
xumoyunjonqoychiyev31-boop <xumoyunjonqoychiyev31@gmail.com>
2026-08-05 14:06:11 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,11 +2,30 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from database.db import add_user
+from keyboards.menu import main_menu
+
 router = Router()
+
 
 @router.message(CommandStart())
 async def start(message: Message):
+
+    await add_user(
+        message.from_user.id,
+        message.from_user.full_name,
+        message.from_user.username or ""
+    )
+
+    text = f"""
+👋 Assalomu alaykum <b>{message.from_user.full_name}</b>!
+
+📅 Daily Plan Botga xush kelibsiz.
+
+Quyidagi menyudan foydalanishingiz mumkin.
+"""
+
     await message.answer(
-        f"Assalomu alaykum, <b>{message.from_user.full_name}</b>! 👋\n\n"
-        "Daily Plan Botga xush kelibsiz."
+        text,
+        reply_markup=main_menu
     )
